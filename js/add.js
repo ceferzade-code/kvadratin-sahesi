e1e1a6b7b621a20de58422a97142343175ed1c56
ceferzade-code <isa.ceferzade96@mail.ru>
2024-02-05 01:07:09 +0400

    kubun sahesi

--- js/add.js
+++ js/add.js
@@ -4,5 +4,10 @@ let y1 = prompt ("y1 neche?");
 let y2 = prompt("y2 neche?");
 alert(`sahesi: ${(x2-x1)*(y2-y1)} , peremetri: ${((x2-x1)+(y2-y1))*2}`);
 
+let z1 = prompt("z1 neche?");
+let z2 = prompt("z2 neche?");
+alert(`kubub sahesi:${(x2-x1)*(y2-y1)*(z2-z1)}`);
+
+
 
 

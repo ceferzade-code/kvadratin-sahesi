9b2cf052710d0bc5e37c24faa9e9de4f840025a7
ceferzade-code <isa.ceferzade96@mail.ru>
2024-02-05 00:32:36 +0400

    kvadrat sahe

---
+++ js/add.js
@@ -0,0 +1,8 @@
+let x1 = prompt("x1 neche?");
+let x2 = prompt("x2 dneche?");
+let y1 = prompt ("y1 neche?");
+let y2 = prompt("y2 neche?");
+alert(`sahesi: ${(x2-x1)*(y2-y1)} , peremetri: ${((x2-x1)+(y2-y1))*2}`);
+
+
+

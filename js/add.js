d8a10a79c82d22784f70ac7edc0ecea3ab66d9d2
ceferzade-code <isa.ceferzade96@mail.ru>
2024-02-05 22:48:50 +0400

    uchbucaqin, silindirin ve cevreinin sahesi.

--- js/add.js
+++ js/add.js
@@ -1,13 +1,24 @@
-let x1 = prompt("x1 neche?");
-let x2 = prompt("x2 dneche?");
-let y1 = prompt ("y1 neche?");
-let y2 = prompt("y2 neche?");
-alert(`sahesi: ${(x2-x1)*(y2-y1)} , peremetri: ${((x2-x1)+(y2-y1))*2}`);
+// let x1 = prompt("x1 neche?");
+// let x2 = prompt("x2 dneche?");
+// let y1 = prompt ("y1 neche?");
+// let y2 = prompt("y2 neche?");
+// alert(`sahesi: ${(x2-x1)*(y2-y1)} , peremetri: ${((x2-x1)+(y2-y1))*2}`);
 
-let z1 = prompt("z1 neche?");
-let z2 = prompt("z2 neche?");
-alert(`kubub sahesi:${(x2-x1)*(y2-y1)*(z2-z1)}`);
+// let z1 = prompt("z1 neche?");
+// let z2 = prompt("z2 neche?");
+// alert(`kubub sahesi:${(x2-x1)*(y2-y1)*(z2-z1)}`);
 
 
+//uchbucaqin sahesi:
+//  let x1 = prompt("x1 qiymeti:");
+//  let x2 = prompt("x2 qiymeti:");
+//  let y = prompt("y qiymeti:");
+//  alert(`uchnucaqin sahesi: ${((x2-x1)*y)/2}`);
 
+
+//silindirin sahesi:
+let r = prompt("r?");
+let h = prompt("h?");
+let p = 3.14;
+alert(`slindirin sahesi: ${r**2*p*h}, chevrenin sahesi:${r**2*p}`);
 
